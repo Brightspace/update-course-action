374abfe0543f406a560d2afeea2b2ad6ed522931
Kristian Horwood <Kristian.Horwood@d2l.com>
2020-07-13 12:23:35 -0400

    Abstract Valence details away

--- src/api/valence-api.js
+++ src/api/valence-api.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const FormData = require('form-data');
 const { LEVersion, LPVersion } = require('../constants');
+const ContentFactory = require('../utility/content-factory');
 
 module.exports = class ValenceApi {
 	constructor(
@@ -11,9 +13,9 @@ module.exports = class ValenceApi {
 		this._fetch = fetch;
 	}
 
-	async getContent(instanceUrl, orgUnit, module = null) {
+	async _getContent(instanceUrl, orgUnit, module = null) {
 		const url = module
-			? new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${module.Id}/structure/`, instanceUrl)
+			? new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${module.Id || module.id}/structure/`, instanceUrl)
 			: new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/root/`, instanceUrl);
 		const signedUrl = this._valence.createAuthenticatedUrl(url, 'GET');
 
@@ -24,12 +26,29 @@ module.exports = class ValenceApi {
 		return response.json();
 	}
 
-	async createModule(instanceUrl, orgUnit, module, parentModule = null) {
+	async assertModule(instanceUrl, orgUnit, module, parentModule = null) {
+		const contents = await this._getContent(instanceUrl, orgUnit, parentModule);
+		const self = Array.isArray(contents) && contents.find(x => x.Type === 0 && x.Title === module.title);
+
+		if (!self) {
+			return this._createModule(instanceUrl, orgUnit, module, parentModule);
+		}
+
+		return this._updateModule(instanceUrl, orgUnit, self, module);
+	}
+
+	async _createModule(instanceUrl, orgUnit, module, parentModule = null) {
 		const url = parentModule
-			? new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${parentModule.Id}/structure/`, instanceUrl)
+			? new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${parentModule.Id || parentModule.id}/structure/`, instanceUrl)
 			: new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/root/`, instanceUrl);
 
 		const signedUrl = this._valence.createAuthenticatedUrl(url, 'POST');
+
+		const createModule = ContentFactory.createModule({
+			title: module.title,
+			description: module.description,
+			dueDate: module.dueDate
+		});
 
 		const response = await this._fetch(
 			signedUrl,
@@ -38,17 +57,40 @@ module.exports = class ValenceApi {
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(module)
+				body: JSON.stringify(createModule)
 			});
 
 		this._assertResponse(response);
 
-		return response.json();
+		const json = await response.json();
+
+		return this._convertModule(module, json.Id);
 	}
 
-	async updateModule(instanceUrl, orgUnit, module) {
-		const url = new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${module.Id}`, instanceUrl);
+	async _updateModule(instanceUrl, orgUnit, self, module) {
+		const url = new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${self.Id}`, instanceUrl);
 		const signedUrl = this._valence.createAuthenticatedUrl(url, 'PUT');
+
+		let isDirty = false;
+		if (self.Title !== module.title) {
+			self.Title = module.title;
+			self.ShortTitle = module.title;
+			isDirty = true;
+		}
+
+		if (self.Description.Html !== module.description) {
+			self.Description.Html = module.description;
+			isDirty = true;
+		}
+
+		if (self.ModuleDueDate !== module.dueDate) {
+			self.ModuleDueDate = module.dueDate || null;
+			isDirty = true;
+		}
+
+		if (!isDirty) {
+			return module;
+		}
 
 		const response = await this._fetch(
 			signedUrl,
@@ -57,12 +99,133 @@ module.exports = class ValenceApi {
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify(module)
+				body: JSON.stringify(self)
 			});
 
 		this._assertResponse(response);
 
-		return module;
+		return this._convertModule(module, self.Id);
+	}
+
+	async assertTopic(instanceUrl, orgUnit, { module, topic, data }) {
+		const contents = await this._getContent(instanceUrl, orgUnit, module);
+		const self = Array.isArray(contents) && contents.find(x => x.Type === 1 && x.Title === topic.title);
+
+		if (!self) {
+			return this._createTopic(instanceUrl, orgUnit, { module, topic, data });
+		}
+
+		await this._updateTopic(instanceUrl, orgUnit, self, topic);
+		return this._updateTopicFile(instanceUrl, orgUnit, { self, topic, data });
+	}
+
+	async _createTopic(instanceUrl, orgUnit, { module, topic, data }) {
+		const url = new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/modules/${module.Id || module.id}/structure/`, instanceUrl);
+		const signedUrl = this._valence.createAuthenticatedUrl(url, 'POST');
+
+		const fileName = topic.fileName.replace(/.md$/, '.html');
+		const createTopic = ContentFactory.createTopic({
+			title: topic.title,
+			url: `${orgUnit.Path}${fileName}`,
+			dueDate: topic.dueDate,
+			isHidden: topic.type === 'resource',
+			isExempt: !topic.isRequired
+		});
+
+		const formData = new FormData();
+		formData.append(
+			'',
+			JSON.stringify(createTopic),
+			{ contentType: 'application/json' }
+		);
+		formData.append(
+			'',
+			data,
+			{ filename: fileName }
+		);
+
+		const response = await this._fetch(
+			signedUrl,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': `multipart/mixed; boundary=${formData.getBoundary()}`
+				},
+				body: formData.getBuffer().toString('utf-8')
+			});
+
+		this._assertResponse(response);
+
+		const json = await response.json();
+
+		return this._convertTopic(topic, json.Id);
+	}
+
+	async _updateTopic(instanceUrl, orgUnit, self, topic) {
+		const url = new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/topics/${self.Id}`, instanceUrl);
+		const signedUrl = this._valence.createAuthenticatedUrl(url, 'PUT');
+
+		let isDirty = false;
+		if (self.Title !== topic.title) {
+			self.Title = topic.title;
+			self.ShortTitle = topic.title;
+			isDirty = true;
+		}
+
+		if (self.DueDate !== topic.DueDate) {
+			self.DueDate = topic.dueDate || null;
+			isDirty = true;
+		}
+
+		if (self.IsExempt === topic.isRequired) {
+			self.IsExempt = !topic.isRequired;
+			isDirty = true;
+		}
+
+		if (!isDirty) {
+			return topic;
+		}
+
+		const response = await this._fetch(
+			signedUrl,
+			{
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(self)
+			});
+
+		this._assertResponse(response);
+
+		return this._convertTopic(topic, self.Id);
+	}
+
+	async _updateTopicFile(instanceUrl, orgUnit, { self, topic, data }) {
+		const url = new URL(`/d2l/api/le/${LEVersion}/${orgUnit.Identifier}/content/topics/${self.Id}/file`, instanceUrl);
+		const signedUrl = this._valence.createAuthenticatedUrl(url, 'PUT');
+
+		const fileName = topic.fileName.replace(/.md$/, '.html');
+		const formData = new FormData();
+		formData.append(
+			'file',
+			data,
+			{ filename: `${fileName}` }
+		);
+
+		const response = await this._fetch(
+			signedUrl,
+			{
+				method: 'PUT',
+				headers: {
+					'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`
+				},
+				body: formData
+			});
+
+		this._assertResponse(response);
+
+		return this._convertTopic(topic, self.Id);
 	}
 
 	async getOrgUnit(instanceUrl, orgUnitId) {
@@ -93,5 +256,19 @@ module.exports = class ValenceApi {
 		}
 
 		throw new Error(response.statusText);
+	}
+
+	_convertModule(module, id) {
+		return {
+			...module,
+			id
+		};
+	}
+
+	_convertTopic(topic, id) {
+		return {
+			...topic,
+			id
+		};
 	}
 };
--- src/test/api/valence-api.test.js
+++ src/test/api/valence-api.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fetchMock = require('fetch-mock');
 const test = require('ava');
+const fetchMock = require('fetch-mock');
 
 const ValenceApi = require('../../api/valence-api');
 
@@ -12,38 +12,39 @@ const valenceMock = {
 };
 
 const orgUnit = {
-	Identifier: 123
+	Identifier: 123,
+	Path: '/content/course123/'
 };
 
-test('getContent retrieves root content items', async t => {
+test('_getContent retrieves root content items', async t => {
 	const data = require('../../testdata/get-content-root.json');
 	const fetch = fetchMock.sandbox();
 	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/root/' }, data);
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	const result = await api.getContent('https://example.com/', orgUnit);
+	const result = await api._getContent('https://example.com/', orgUnit);
 
 	t.deepEqual(result, data.body);
 });
 
-test('getContent throws on HTTP error', async t => {
+test('_getContent throws on HTTP error', async t => {
 	const fetch = fetchMock.sandbox();
 	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/root/' }, 400);
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	await t.throwsAsync(async () => api.getContent('https://example.com/', orgUnit), { instanceOf: Error, message: 'Bad Request' });
+	await t.throwsAsync(async () => api._getContent('https://example.com/', orgUnit), { instanceOf: Error, message: 'Bad Request' });
 });
 
-test('getContent retrieves module content items', async t => {
+test('_getContent retrieves module content items', async t => {
 	const data = require('../../testdata/get-content-module.json');
 	const fetch = fetchMock.sandbox();
 	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/modules/456/structure/' }, data);
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	const result = await api.getContent('https://example.com/', orgUnit, { Id: 456 });
+	const result = await api._getContent('https://example.com/', orgUnit, { Id: 456 });
 
 	t.deepEqual(result, data.body);
 });
@@ -69,20 +70,75 @@ test('getOrgUnit throws on HTTP error', async t => {
 	await t.throwsAsync(async () => api.getOrgUnit('https://example.com/', 123), { instanceOf: Error, message: 'Not Found' });
 });
 
-test('createModule returns created module', async t => {
+test('assertModule calls _createModule', async t => {
 	const module = {
-		Title: 'Title',
-		ShortTitle: 'ShortTitle',
-		Type: 0,
-		ModuleStartDate: null,
-		ModuleEndDate: null,
-		ModuleDueDate: '2020-01-01T00:00:000.0Z',
-		IsHidden: false,
-		IsLocked: false,
-		Description: {
-			Content: '<html></html>',
-			Type: 'Html'
-		}
+		title: 'Title',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:000.0Z',
+		description: '<html></html>'
+	};
+
+	const fetch = fetchMock.sandbox();
+	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/root/' }, { status: 200, body: [] });
+
+	const api = new ValenceApi(valenceMock, fetch);
+	api._createModule = (url, ou, m, pm) => {
+		t.is(url, 'https://example.com/');
+		t.deepEqual(ou, orgUnit);
+		t.deepEqual(m, module);
+		t.is(pm, null);
+
+		return {
+			id: 1,
+			...m
+		};
+	};
+
+	const result = await api.assertModule('https://example.com/', orgUnit, module);
+
+	t.deepEqual(result, {
+		id: 1,
+		...module
+	});
+});
+
+test('assertModule calls _updateModule', async t => {
+	const module = {
+		id: 1,
+		title: 'Module',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:000.0Z',
+		description: '<html></html>'
+	};
+
+	const fetch = fetchMock.sandbox();
+	const response = require('../../testdata/get-content-root.json');
+	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/root/' }, response);
+
+	const api = new ValenceApi(valenceMock, fetch);
+	api._updateModule = (url, ou, s, m) => {
+		t.is(url, 'https://example.com/');
+		t.deepEqual(ou, orgUnit);
+		t.deepEqual(s, response.body[0]);
+		t.deepEqual(m, module);
+
+		return m;
+	};
+
+	const result = await api.assertModule('https://example.com/', orgUnit, module);
+
+	t.deepEqual(result, {
+		id: 1,
+		...module
+	});
+});
+
+test('_createModule returns created module', async t => {
+	const module = {
+		title: 'Title',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:000.0Z',
+		description: '<html></html>'
 	};
 
 	const response = require('../../testdata/post-module-response.json');
@@ -91,25 +147,20 @@ test('createModule returns created module', async t => {
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	const result = await api.createModule('https://example.com/', orgUnit, module);
+	const result = await api._createModule('https://example.com/', orgUnit, module);
 
-	t.deepEqual(result, response.body);
+	t.deepEqual(result, {
+		id: 1,
+		...module
+	});
 });
 
-test('createModule returns created submodule', async t => {
+test('_createModule returns created submodule', async t => {
 	const module = {
-		Title: 'Title',
-		ShortTitle: 'ShortTitle',
-		Type: 0,
-		ModuleStartDate: null,
-		ModuleEndDate: null,
-		ModuleDueDate: '2020-01-01T00:00:000.0Z',
-		IsHidden: false,
-		IsLocked: false,
-		Description: {
-			Content: '<html></html>',
-			Type: 'Html'
-		}
+		title: 'Title',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:000.0Z',
+		description: '<html></html>'
 	};
 
 	const response = require('../../testdata/post-submodule-response.json');
@@ -118,12 +169,15 @@ test('createModule returns created submodule', async t => {
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	const result = await api.createModule('https://example.com/', orgUnit, module, { Id: 1 });
+	const result = await api._createModule('https://example.com/', orgUnit, module, { Id: 1 });
 
-	t.deepEqual(result, response.body);
+	t.deepEqual(result, {
+		id: 2,
+		...module
+	});
 });
 
-test('createModule throws on HTTP error', async t => {
+test('_createModule throws on HTTP error', async t => {
 	const module = {};
 
 	const fetch = fetchMock.sandbox();
@@ -131,24 +185,32 @@ test('createModule throws on HTTP error', async t => {
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	await t.throwsAsync(async () => api.createModule('https://example.com/', orgUnit, module), { instanceOf: Error, message: 'Bad Request' });
+	await t.throwsAsync(async () => api._createModule('https://example.com/', orgUnit, module), { instanceOf: Error, message: 'Bad Request' });
 });
 
-test('updateModule returns updated module', async t => {
-	const module = {
+test('_updateModule returns updated module', async t => {
+	const self = {
 		Id: 1,
-		Title: 'NewTitle',
-		ShortTitle: 'NewShortTitle',
+		Title: 'Title',
+		ShortTitle: 'Title',
 		Type: 0,
 		ModuleStartDate: null,
 		ModuleEndDate: null,
-		ModuleDueDate: '2020-01-01T00:00:000.0Z',
+		ModuleDueDate: '2020-01-01T00:00:00.000Z',
+		ParentModuleId: null,
 		IsHidden: false,
 		IsLocked: false,
 		Description: {
-			Content: '<html>Update!</html>',
-			Type: 'Html'
+			Html: '<html></html>'
 		}
+	};
+
+	const module = {
+		id: 1,
+		title: 'NewTitle',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:00.000Z',
+		description: '<html>Update!</html>'
 	};
 
 	const fetch = fetchMock.sandbox();
@@ -156,9 +218,196 @@ test('updateModule returns updated module', async t => {
 
 	const api = new ValenceApi(valenceMock, fetch);
 
-	const result = await api.updateModule('https://example.com/', orgUnit, module);
+	const result = await api._updateModule('https://example.com/', orgUnit, self, module);
 
 	t.deepEqual(result, module);
+});
+
+test('assertTopic calls _createTopic', async t => {
+	const testModule = {
+		id: 1,
+		title: 'Title',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:000.0Z',
+		description: '<html></html>'
+	};
+
+	const testTopic = {
+		title: 'Topic',
+		type: 'topic',
+		fileName: 'module/topic.md'
+	};
+
+	const testData = '<html></html>';
+
+	const fetch = fetchMock.sandbox();
+	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/modules/1/structure/' }, { status: 200, body: [] });
+
+	const api = new ValenceApi(valenceMock, fetch);
+	api._createTopic = (url, ou, { module, topic, data }) => {
+		t.is(url, 'https://example.com/');
+		t.deepEqual(ou, orgUnit);
+		t.deepEqual(module, testModule);
+		t.deepEqual(topic, testTopic);
+		t.deepEqual(data, testData);
+
+		return {
+			id: 2,
+			...topic
+		};
+	};
+
+	const result = await api.assertTopic('https://example.com/', orgUnit, { module: testModule, topic: testTopic, data: testData });
+
+	t.deepEqual(result, {
+		id: 2,
+		...testTopic
+	});
+});
+
+test('assertTopic calls _updateTopic', async t => {
+	const testModule = {
+		id: 1,
+		title: 'Module',
+		type: 'module',
+		dueDate: '2020-01-01T00:00:000.0Z',
+		description: '<html></html>'
+	};
+
+	const testTopic = {
+		id: 2,
+		title: 'Test Topic',
+		type: 'Test topic',
+		fileName: 'module/topic.md'
+	};
+
+	const testData = '<html></html>';
+
+	const fetch = fetchMock.sandbox();
+	const response = require('../../testdata/get-modules-response.json');
+	fetch.get({ url: 'https://example.com/d2l/api/le/1.44/123/content/modules/1/structure/' }, response);
+
+	const api = new ValenceApi(valenceMock, fetch);
+	api._updateTopic = (url, ou, self, topic) => {
+		t.is(url, 'https://example.com/');
+		t.deepEqual(ou, orgUnit);
+		t.deepEqual(self, response.body[0]);
+		t.deepEqual(topic, testTopic);
+
+		return topic;
+	};
+
+	api._updateTopicFile = (url, ou, { self, topic, data }) => {
+		t.is(url, 'https://example.com/');
+		t.deepEqual(ou, orgUnit);
+		t.deepEqual(self, response.body[0]);
+		t.deepEqual(topic, testTopic);
+		t.is(data, testData);
+
+		return topic;
+	};
+
+	const result = await api.assertTopic('https://example.com/', orgUnit, { module: testModule, topic: testTopic, data: testData });
+
+	t.deepEqual(result, {
+		id: 2,
+		...testTopic
+	});
+});
+
+test('_createTopic returns created topic', async t => {
+	const topic = {
+		title: 'Test Topic',
+		type: 'topic',
+		fileName: 'test-module/test-topic.md',
+		isRequired: true
+	};
+
+	const response = require('../../testdata/post-topic-response.json');
+	const fetch = fetchMock.sandbox();
+	fetch.post((url, options) => {
+		if (url !== 'https://example.com/d2l/api/le/1.44/123/content/modules/1/structure/') {
+			return false;
+		}
+
+		const contentType = options.headers['Content-Type'];
+		if (!contentType.includes('multipart/mixed; boundary=')) {
+			return false;
+		}
+
+		const boundary = options.headers['Content-Type'].match(/boundary=(?<boundary>.*?)$/).groups.boundary;
+
+		return options.body === `--${boundary}\r\nContent-Disposition: form-data; name=""\r\nContent-Type: application/json\r\n\r\n`
+			+ '{"Title":"Test Topic","ShortTitle":"Test Topic","Type":1,"TopicType":1,"StartDate":null,"EndDate":null,"DueDate":null,"Url":"/content/course123/test-module/test-topic.html","IsHidden":false,"IsLocked":false,"IsExempt":false}\r\n'
+			+ `--${boundary}\r\nContent-Disposition: form-data; name=""; filename="test-topic.html"\r\nContent-Type: text/html\r\n\r\n<h1></h1>\r\n`
+			+ `--${boundary}--\r\n`;
+	}, response);
+
+	const api = new ValenceApi(valenceMock, fetch);
+
+	const result = await api._createTopic('https://example.com/', orgUnit, { module: { id: 1 }, topic, data: '<h1></h1>' });
+
+	t.deepEqual(result, {
+		id: 2,
+		...topic
+	});
+});
+
+test('_createTopic throws on HTTP error', async t => {
+	const topic = {
+		title: 'Test Topic',
+		type: 'topic',
+		fileName: 'test-module/test-topic.md',
+		isRequired: true
+	};
+
+	const fetch = fetchMock.sandbox();
+	fetch.post('*', 400);
+
+	const api = new ValenceApi(valenceMock, fetch);
+
+	await t.throwsAsync(
+		async () => api._createTopic('https://example.com/', orgUnit, { module: { id: 1 }, topic, fileName: 'test-module/test-topic.html', data: '<h1></h1>' }),
+		{ instanceOf: Error, message: 'Bad Request' }
+	);
+});
+
+test('_updateTopic returns updated topic', async t => {
+	const self = {
+		Id: 2,
+		Title: 'Topic',
+		ShortTitle: 'Topic',
+		Type: 1,
+		TopicType: 1,
+		StartDate: null,
+		EndDate: null,
+		DueDate: null,
+		Url: '/content/course123/test-module/test-topic.html',
+		IsHidden: false,
+		IsLocked: false,
+		IsExempt: true
+	};
+
+	const topic = {
+		id: 2,
+		title: 'Test Topic',
+		type: 'topic',
+		fileName: 'test-module/test-topic.md'
+	};
+
+	const fetch = fetchMock.sandbox();
+	fetch.put(require('../../testdata/put-topic-request.json'), 200);
+
+	const api = new ValenceApi(valenceMock, fetch);
+
+	const result = await api._updateTopic('https://example.com/', orgUnit, self, topic);
+
+	t.deepEqual(result, {
+		id: 2,
+		title: 'Test Topic',
+		type: 'topic',
+		fileName: 'test-module/test-topic.md'
+	});
 });
 
 test('whoAmI retrieves user', async t => {
