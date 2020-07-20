0f506c0be34496c689d086440696e5618d90e652
Peter Lloyd <Peter.Lloyd@D2L.com>
2020-07-20 13:47:43 -0400

    fixing constructor inputs

--- src/index.js
+++ src/index.js
@@ -5,18 +5,17 @@ const ValenceAuth = require('./auth/valence-auth');
 const UploadCourseContent = require('./upload-course-content');
 const Validator = require('./validator');
 const ValenceApi = require('./api/valence-api');
+const FileHandler = require('./utility/file-handler');
 const LinkRewriter = require('./link-rewriter');
 
 async function run() {
 	const validator = new Validator(core);
 	const valence = new ValenceAuth(validator.getValenceInput());
 	const api = new ValenceApi(valence, validator.getIsDryRun());
-	const inputs = {
-		contentDirectory: validator.getContentDirectory(),
-		manifestPath: validator.getManifestPath()
-	};
-	const uploader = new UploadCourseContent(inputs, api);
-	const rewriter = new LinkRewriter(inputs.contentDirectory, api);
+	const fileHandler = new FileHandler(validator.getContentDirectory());
+
+	const uploader = new UploadCourseContent(validator.getManifestPath(), fileHandler, api);
+	const rewriter = new LinkRewriter(fileHandler, api);
 
 	const manifest = await uploader.uploadCourseContent(validator.getInstanceDomain(), validator.getCourseOrgUnitId());
 
